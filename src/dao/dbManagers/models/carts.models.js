d0699bbf65bb31bbf55c8f734601dd6024b53390
Gabriel Lagrotteria <gabriellagrotteria18@gmail.com>
2023-11-04 15:53:13 -0300

    middleware populate agregado al modelo cart

--- src/dao/dbManagers/models/carts.models.js
+++ src/dao/dbManagers/models/carts.models.js
@@ -36,4 +36,9 @@ const cartsSchema = new mongoose.Schema({
 
 });
 
+cartsSchema.pre('find', function(){
+    this.populate('products.product');
+})
+
+
 export const cartsModel = mongoose.model(cartsCollection, cartsSchema)
