cf373b2a658ed71484ff7cab154d673959d1d9ad
Gabriel Lagrotteria <gabriellagrotteria18@gmail.com>
2023-11-06 17:06:22 -0300

    Falta que funcionen los botones del front

--- src/public/js/products.js
+++ src/public/js/products.js
@@ -1,6 +1,64 @@
 // Socket comunica con servidor
 const socket = io();
 
+const carritoId = '6548f637d8891916f4b7065b';
+
+
+// Función para agregar un producto a un carrito
+async function agregarProductoAlCarrito(carritoId) {
+    try {
+        // Realiza la solicitud al servidor para obtener el carrito
+        const response = await fetch(`/api/carts/${carritoId}`);
+
+        if (!response.ok) {
+            throw new Error('Error en la solicitud al servidor');
+        }
+        // Convierte el cuerpo de la respuesta en un objeto JSON
+        const data = await response.json();
+
+        const cartData = data.payload;
+
+        console.log(cartData)
+        // Buscar el producto en el carrito por el ID del producto
+        const existingProduct = cartData.products.find(p => p.product._id);
+
+        console.log(existingProduct)
+
+    if (existingProduct) {
+      // Si el producto ya existe en el carrito, incrementa la cantidad
+      existingProduct.quantity += 1;
+    } else {
+      // Crea un nuevo objeto de producto utilizando el ID del producto
+      const addedProduct = {
+        product: {
+          _id: productoId,
+          quantity: 1
+        }
+      };
+      // Agrega el producto al arreglo "products" del carrito
+      cartData.products.push(addedProduct);
+    }
+
+        // Realizar una solicitud fetch para actualizar el carrito
+        const updateResponse = await fetch(`/api/carts/${carritoId}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(cartData)
+        });
+
+        if (updateResponse.status === 200) {
+            console.log('Producto añadido al carrito');
+            console.log(cartData); // Aquí puedes hacer lo que desees con el carrito actualizado
+        };
+
+    } catch (error) {
+        console.error('Error:', error.message);
+    }
+}
+
+
 //acá pondré los productos que me pasa el cliente
 const container = document.getElementById('container');
 
@@ -18,13 +76,19 @@ socket.on('showProducts', data => {
                 <li>stock: ${product.stock}</li>
                 <li>category: ${product.category}</li>
                 <li>id: ${product._id}</li>
+                <li><button class="add-to-cart" id="${this._id}">Agregar al Carrito</button></li>
             </ul>
         `
-    })
+    });
+    // Agregar un manejador de eventos a los botones "Agregar al Carrito"
+    const addToCartButtons = document.querySelectorAll('.add-to-cart');
+    addToCartButtons.forEach(button => {
+      button.addEventListener('click', () => {
+        const productoId = button.getAttribute('id')
+        agregarProductoAlCarrito(carritoId, productoId);
+        alert("producto añadido")
+      });
+    });
 })
 
-
-
-
-
-
+console.log("Carrito ID:", carritoId);
--- src/routes/web/views.router.js
+++ src/routes/web/views.router.js
@@ -75,7 +75,55 @@ router.get('/realTimeProducts', async (req, res) => {
 });
 
 router.get('/products', async (req, res) => { 
-    res.render('products', { products: await prodManager.getAll() });
+  
+    const limit = parseInt(req.query.limit) || 10; 
+    const page = parseInt(req.query.page) || 1;    
+    const sort = req.query.sort || null; 
+    const query = req.query.query || null;
+    const queryValue = req.query.queryValue || null;
+
+     // Configurar las opciones de búsqueda
+     const options = {
+        limit,
+        page,
+        lean: true
+    };
+
+    if (sort !== null) {
+    options.sort = sort; // Aplica el valor de sort solo si no es null
+    }
+
+    const filter = {};
+
+    if (query !== null && queryValue !== null) {
+        filter[query] = queryValue; // Aplica el valor de sort solo si no es null
+        }
+
+     // Se agrega lógica para determinar el orden
+     if (sort !== null) {
+        if (sort.toLowerCase() === 'asc') {
+            
+            options.sort = { precio: 'asc' };
+        } else if (sort.toLowerCase() === 'desc') {
+            options.sort = { precio: 'desc' };
+        }
+    }
+
+    // se agregan parametros de paginacion
+
+    const {docs, hasPrevPage, hasNextPage, nextPage, prevPage} = await productsModel.paginate(filter, options);
+    
+    res.render('products', { 
+        products: docs,
+        hasPrevPage,
+        hasNextPage,
+        nextPage,
+        prevPage,
+        limit: limit,
+        page,
+        query,
+        sort });
+
 });
 
 router.get('/realTimeCarts', async (req, res) => { 
